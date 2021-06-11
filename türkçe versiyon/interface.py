9bfcf62a990ee066293509f9d3180d33b648aeab
BAHADIRking <b123qweasdzxc@gmail.com>
2021-06-12 00:56:36 +0300

    modern ui update

--- türkçe versiyon/interface.py
+++ türkçe versiyon/interface.py
@@ -10,19 +10,61 @@ class Ui_MainWindow(object):
         MainWindow.setAnimated(True)
         MainWindow.setWindowIcon(QtGui.QIcon(rf"{os.getcwd()}\son5.ico"))
         MainWindow.setStyleSheet(";\n"
-                                 "background-color: rgb(54,54,54);")
+                                 "background-color: rgb(54,54,54);"
+                                 "border: 1px solid rgb(54,54,54);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.lyrics = QtWidgets.QTextBrowser(self.centralwidget)
         self.lyrics.setObjectName("lyrics")
-        self.lyrics.setStyleSheet("color: rgb(255,255,255);")
+        self.scrrolll = QtWidgets.QScrollBar(self.centralwidget)
+        self.scrrolll.setStyleSheet("QScrollBar:vertical" "{"              
+    "border: 1px solid rgb(54,54,54);"
+    "background:rgb(54,54,54);"
+    "width:14px;"
+                                    
+    "margin: 0px 0px 0px 0px;"
+    "}"
+    "QScrollBar::handle:vertical {"
+    "background: qlineargradient(x1:0, y1:0, x2:1, y2:0,"
+    "stop: 0 #202225, stop: 0.5 #202225, stop:1 #202225);"
+    "min-height: 0px;"
+    "margin: 0px 1px 0px 1px;"                               
+    "width: 10px;"
+    "border-width: 2px;" 
+    "border-radius: 5px;"
+        
+    "}"
+    "QScrollBar::add-line:vertical {"
+    "background: qlineargradient(x1:0, y1:0, x2:1, y2:0,"
+    "stop: 0 rgb(32, 47, 130), stop: 0.5 rgb(32, 47, 130),  stop:1 rgb(32, 47, 130));"
+    "height: 0px;"
+    "subcontrol-position: bottom;"
+    "subcontrol-origin: margin;"
+    "}"
+    "QScrollBar::sub-line:vertical {"
+    "background: qlineargradient(x1:0, y1:0, x2:1, y2:0,"
+    "stop: 0  rgb(32, 47, 130), stop: 0.5 rgb(32, 47, 130),  stop:1 rgb(32, 47, 130));"
+    "height: 0 px;"
+    "subcontrol-position: top;"
+    "subcontrol-origin: margin;"
+    "}"
+    "QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical" "{"
+    "background: #2e3338;"
+    "border-width: 2px;" 
+    "border-radius: 5px;"
+                                    
+    "}"
+    )
+        self.lyrics.setVerticalScrollBar(self.scrrolll)
+        self.lyrics.setStyleSheet("color: rgb(255,255,255)")
         self.lyrics.setFont(QFont('Times', 10))
 
         self.song_name = QtWidgets.QLabel(self.centralwidget)
         self.song_name.setText("")
         self.song_name.setObjectName("song_name")
         self.song_name.setStyleSheet("color: rgb(255,255,255);")
+        self.song_name.setTextInteractionFlags(Qt.TextSelectableByMouse)
 
         self.bahadir = QtWidgets.QLabel(self.centralwidget)
         self.bahadir.setObjectName("bahadir")
@@ -30,6 +72,10 @@ class Ui_MainWindow(object):
         self.bahadir2 = QtWidgets.QLabel(self.centralwidget)
         self.bahadir2.setObjectName("bahadir2")
         self.bahadir2.setStyleSheet("color: rgb(255,255,255);")
+
+        self.bahadir3 = QtWidgets.QLabel(self.centralwidget)
+        self.bahadir3.setObjectName("bahadir3")
+        self.bahadir3.setStyleSheet("color: rgb(255,255,255);")
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.v_box = QtWidgets.QVBoxLayout()
@@ -39,8 +85,8 @@ class Ui_MainWindow(object):
         self.h_box.addWidget(self.bahadir, alignment=Qt.AlignRight, stretch=1)
         self.h_box.addWidget(self.bahadir2, alignment=Qt.AlignRight, stretch=0)
 
-
         self.v_box.addLayout(self.h_box)
+        self.v_box.addWidget(self.bahadir3, alignment=Qt.AlignBottom, stretch=0)
         self.v_box.addWidget(self.lyrics)
         self.centralwidget.setLayout(self.v_box)
 
@@ -51,7 +97,9 @@ class Ui_MainWindow(object):
 
         MainWindow.setWindowTitle(_translate("MainWindow", "soptify lyrics-TR"))
         self.song_name.setText(_translate("MainWindow", "şarkı bekleniyor..."))
-        self.song_name.setFont(QFont('Times', 10))
+        self.song_name.setFont(QFont('Times', 12,weight=QtGui.QFont.Bold))
+        self.bahadir3.setText(_translate("MainWindow", "_"*(len("şarkı bekleniyor...")-2)))
+        self.bahadir3.setFont(QFont('Times',12,weight=QtGui.QFont.Bold))
         self.bahadir2.setText(_translate("MainWindow", "tarafından geliştirildi."))
         self.bahadir2.setFont(QFont('Times', 10))
         self.bahadir.setText("<a href='https://github.com/BAHADIR54' style='color:{}'> BAHADIR54</a>".format("#FFFFFF"))
--- türkçe versiyon/main.py
+++ türkçe versiyon/main.py
@@ -7,6 +7,7 @@ class SongSelection(QThread):
     song_name = pyqtSignal(str)
     lyrics = pyqtSignal(str)
     error = pyqtSignal(str)
+    le = pyqtSignal(int)
     def __init__(self):
         super().__init__()
     def run(self):
@@ -18,10 +19,11 @@ class SongSelection(QThread):
 
                 if song_info[0] != flag and song_info[1] != '' and song_info != "spotify kapalı":
                     data = f"{song_info[0]} {song_info[1]}"
-
+                    le = len(data)
                     lyrics = functions.get_lyrics(song=song_info[0], artist=song_info[1].split(",")[0])
                     self.song_name.emit(data)
                     self.lyrics.emit(lyrics)
+                    self.le.emit(le)
                     flag = song_info[0]
                     flag2 = song_info[1]
 
@@ -45,6 +47,7 @@ class MainWindow(QMainWindow):
         self.song.song_name.connect(lambda data: self.ui.song_name.setText(str(data)))
         self.song.lyrics.connect(lambda lyrics: self.ui.lyrics.setText(str(lyrics)))
         self.song.error.connect(lambda error: self.ui.song_name.setText(str(error)))
+        self.song.le.connect(lambda le: self.ui.bahadir3.setText(str(("_"*le))))
         self.song.start()
 
 
